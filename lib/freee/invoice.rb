d383be756485b5f19e786a29084076fcbe427d39
tak-okmt <gcp632dsh@gmail.com>
2023-09-05 18:27:17 +0900

    400エラー回避

--- lib/freee/invoice.rb
+++ lib/freee/invoice.rb
@@ -1,7 +1,11 @@
 module Freee
   class Invoice < Freee::Base
     def self.create(params)
-      Freee.client.post('/iv/invoices', :invoice, params)
+      client = Freee.client
+      client.header["Content-Type"] = 'application/json'
+      client.headers['Accept'] = 'application/json'
+
+      client.post('/iv/invoices', :invoice, params)
     end
   end
 end
